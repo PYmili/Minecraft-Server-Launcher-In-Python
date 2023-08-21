41cd3bab5cdf61f844b4d7ebbc1a01f9cfd31264
Empty <2167335204@qq.com>
2023-08-21 20:28:11 +0800

    'update'

--- MSLIP_lib/Action.py
+++ MSLIP_lib/Action.py
@@ -15,27 +15,35 @@ class Action(ABC):
 
 
 class BackendMethod(Action):
-    def __init__(self, spigot_v: str, xmx: str, xms: str):
+    def __init__(self, ser_name: str = 'Test_1.18',
+                 server_v: str = '1.18',
+                 xmx: str = '4096',
+                 xms: str = '2048',
+                 select_v: str = '1.18',
+                 ):
         """
-        spigot_v:服务器名称+spigot版本,由gui传入,格式:name_version
+        ser_name:服务器名称
+        server_v:服务器框架版本
         xmx:最大内存
         xms:最小内存
         """
-        self.spigot_v = spigot_v
+        self.ser_name = ser_name
+        self.server_v = server_v
+        self.select_v = select_v
+        self.spigot_url = f'https://mcversions.net/download/{self.select_v}'
         self.xmx = xmx
         self.xms = xms
 
     def startServer(self) -> None:
         """此函数由启动服务器事件调用"""
-        with open(r'../Servers/Test_1.18/test.txt', 'r', encoding='utf-8') as fr:
-            f = fr.read()
-            print(f)
-        subprocess.Popen(f'java -{self.xmx} -{self.xms} -jar ../Servers/{self.spigot_v}/spigot', shell=True)
+        subprocess.Popen(f'java -{self.xmx} -{self.xms} -jar ../Servers/{self.ser_name}/server.jar',
+                         shell=True)
 
     def DownloadJar(self) -> None:
         """此方法由下载事件调用"""
+
         pass
 
 
-b = BackendMethod(spigot_v='a', xms='a', xmx='s')
-b.startServer()
+b = BackendMethod()
+b.DownloadJar()
