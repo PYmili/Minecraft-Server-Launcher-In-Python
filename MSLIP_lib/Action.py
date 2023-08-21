d264840ccfc1dfac0a72c45ee0edbc6805df91ce
Empty <2167335204@qq.com>
2023-08-21 18:31:40 +0800

    'update'

--- MSLIP_lib/Action.py
+++ MSLIP_lib/Action.py
@@ -1,3 +1,4 @@
+import subprocess
 from abc import ABC, abstractmethod
 
 
@@ -14,11 +15,27 @@ class Action(ABC):
 
 
 class BackendMethod(Action):
-    def __init__(self, a):
-        self.b = a
+    def __init__(self, spigot_v: str, xmx: str, xms: str):
+        """
+        spigot_v:服务器名称+spigot版本,由gui传入,格式:name_version
+        xmx:最大内存
+        xms:最小内存
+        """
+        self.spigot_v = spigot_v
+        self.xmx = xmx
+        self.xms = xms
 
     def startServer(self) -> None:
-        pass
+        """此函数由启动服务器事件调用"""
+        with open(r'../Servers/Test_1.18/test.txt', 'r', encoding='utf-8') as fr:
+            f = fr.read()
+            print(f)
+        subprocess.Popen(f'java -{self.xmx} -{self.xms} -jar ../Servers/{self.spigot_v}/spigot', shell=True)
 
     def DownloadJar(self) -> None:
+        """此方法由下载事件调用"""
         pass
+
+
+b = BackendMethod(spigot_v='a', xms='a', xmx='s')
+b.startServer()
