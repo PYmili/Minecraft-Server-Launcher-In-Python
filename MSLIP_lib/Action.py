58c23af9af3e898e6da4e4b7cffe99c9f355acaa
PYmili <mc2005wj@163.com>
2023-08-21 14:44:53 +0800

    update Action

---
+++ MSLIP_lib/Action.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class Action(ABC):
+    """所有方法的基类"""
+
+    @abstractmethod
+    def startServer(self) -> None:
+        """启动服务器"""
+    
+    @abstractmethod
+    def DownloadJar(self) -> None:
+        """下载.jar文件"""
