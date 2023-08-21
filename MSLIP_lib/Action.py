9707033e12155f15a65204002b92df017cf1372e
Empty <2167335204@qq.com>
2023-08-21 21:19:29 +0800

    'update'

--- MSLIP_lib/Action.py
+++ MSLIP_lib/Action.py
@@ -1,5 +1,10 @@
+import os
 import subprocess
 from abc import ABC, abstractmethod
+
+import requests
+from fake_user_agent import user_agent
+from lxml import etree
 
 
 class Action(ABC):
@@ -16,21 +21,23 @@ class Action(ABC):
 
 class BackendMethod(Action):
     def __init__(self, ser_name: str = 'Test_1.18',
-                 server_v: str = '1.18',
                  xmx: str = '4096',
                  xms: str = '2048',
                  select_v: str = '1.18',
+                 new_name: str = 'default',
                  ):
         """
-        ser_name:服务器名称
-        server_v:服务器框架版本
+        ser_name:选择启动的服务器名称
+        select_v:选择下载的服务器版本
+        new_name:新建的服务器名称
         xmx:最大内存
         xms:最小内存
         """
         self.ser_name = ser_name
-        self.server_v = server_v
         self.select_v = select_v
+        self.new_name = new_name
         self.spigot_url = f'https://mcversions.net/download/{self.select_v}'
+        self.requests_head = {'User-Agent': user_agent()}
         self.xmx = xmx
         self.xms = xms
 
@@ -41,8 +48,13 @@ class BackendMethod(Action):
 
     def DownloadJar(self) -> None:
         """此方法由下载事件调用"""
-
-        pass
+        req = requests.get(url=self.spigot_url, headers=self.requests_head)
+        html = etree.HTML(req.text)
+        jar_url = html.xpath(f'//a[@download="minecraft_server-{self.select_v}.jar"]/@href')[0]
+        jar_req = requests.get(url=jar_url, headers=self.requests_head)
+        os.mkdir(rf'../Servers/{self.new_name}_{self.select_v}')
+        with open(rf'../Servers/{self.new_name}_{self.select_v}/server.jar', 'ab', encoding='utf-8') as f:
+            f.write(jar_req.content)
 
 
 b = BackendMethod()
